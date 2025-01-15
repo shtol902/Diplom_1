04d899a32eaf75654e0e03238449aca674839623
Alex Shtol <shtol902@yandex.ru>
2025-01-15 22:51:07 +0500

    first commit

---
+++ tests/test_bun.py
@@ -0,0 +1,16 @@
+import pytest
+import data
+
+from praktikum.bun import Bun
+
+class TestBun:
+
+    @pytest.mark.parametrize('name, price', data.bun_data.items())
+    def test_get_name_bun(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
+
+    @pytest.mark.parametrize('name, price', data.bun_data.items())
+    def test_get_price_bun(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_price() == price
---
+++ tests/test_ingredient.py
@@ -0,0 +1,21 @@
+import pytest
+import data
+
+from praktikum.ingredient import Ingredient
+
+class TestIngredient:
+
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    def test_price_ingredient(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_price() == price
+
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    def test_name_ingredient(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_name() == name
+
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    def test_type_ingredient(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_type() == ingredient_type

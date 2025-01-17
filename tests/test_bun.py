4b722e0d471bdfe9e849931d07b2d7778834a938
Alex Shtol <shtol902@yandex.ru>
2025-01-18 01:17:11 +0500

    second commit

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,12 +5,12 @@ from praktikum.bun import Bun
 
 class TestBun:
 
-    @pytest.mark.parametrize('name, price', data.bun_data.items())
+    @pytest.mark.parametrize('name, price', data.buns.items())
     def test_get_name_bun(self, name, price):
         bun = Bun(name, price)
         assert bun.get_name() == name
 
-    @pytest.mark.parametrize('name, price', data.bun_data.items())
+    @pytest.mark.parametrize('name, price', data.buns.items())
     def test_get_price_bun(self, name, price):
         bun = Bun(name, price)
         assert bun.get_price() == price
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -5,17 +5,17 @@ from praktikum.ingredient import Ingredient
 
 class TestIngredient:
 
-    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredients)
     def test_price_ingredient(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_price() == price
 
-    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredients)
     def test_name_ingredient(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_name() == name
 
-    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredient_data)
+    @pytest.mark.parametrize('ingredient_type, name, price', data.ingredients)
     def test_type_ingredient(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_type() == ingredient_type
